8197c8c5eafedffdbddb75fa6d4c2c7819329260
Ajina-p <ajinapr95@gmail.com>
2022-12-01 22:18:05 +0530

    calculator v1

---
+++ calc.js
@@ -0,0 +1,17 @@
+function display(num)
+{
+    console.log(result);
+    result.value+=num
+}
+function clearScreen()
+{
+    result.value=""
+}
+function calculate()
+{
+    result.value=eval(result.value)
+}
+function back()
+{
+    result.value=result.value.slice(0,-1)
+}
